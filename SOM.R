a97446a6929508012d1c22911f3eac5b531b9b87
Thai T. Pham <tmbtw@users.noreply.github.com>
2016-11-23 00:30:10 -0800

    Update SOM.R

--- SOM.R
+++ SOM.R
@@ -1,9 +1,9 @@
 #######################################
-############ Thai T. Pham #############
-######## Stanford University ##########
+#            Thai T. Pham             #
+#        Stanford University          #
 #######################################
 
-# NOTICE: The codes are not optimized for speed #
+# NOTICE: The code is not optimized for speed #
 
 # Self-Organizing Map (SOM) Algorithm
 
